415bc4b5936200449133ba929ed20f30d07c2be6
Disha Pansuriya <dishupansuriya@gmail.com>
2025-01-10 09:35:55 +0530

    changes

--- src/navigation/stack.tsx
+++
@@ -1,8 +0,0 @@
-import { StackScreenProps } from "@react-navigation/stack";
-
-export type ApplicationStackParamList = {
-    homescreen: undefined;
-}
-
-export type ApplicationScreenProps = 
-StackScreenProps<ApplicationStackParamList>
